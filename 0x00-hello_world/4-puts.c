81151b1c7aecef9a1c5402699969f397ea815a74
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-02-16 13:31:19 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+/*
+ *Simple program prints (\"Programming is like building a multilingual puzzle)
+ **/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
