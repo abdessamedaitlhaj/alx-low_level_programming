fde6ede401393ba4b559618e14a3a34718e5559f
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-02-17 10:16:49 -0800

    Print all digit number with some restrictions

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Print digit numbers with some restrictions
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
