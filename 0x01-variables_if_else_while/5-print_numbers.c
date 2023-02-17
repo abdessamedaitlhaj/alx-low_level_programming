ee58c1b9aa53b9fd58146dc7840a748c77e5b5fc
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-02-17 07:27:00 -0800

    Print decila number 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * mian - Print all decimal digit from 0-9
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar("\n");
+	return (0);
+}
