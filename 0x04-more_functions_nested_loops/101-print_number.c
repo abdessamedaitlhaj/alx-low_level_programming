c4040c0197dc0e81b412ab8f7156487ba5c9d53d
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-02-23 14:38:33 -0800

    Print any given integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,7 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, ct;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
@@ -20,16 +20,16 @@ void print_number(int n)
 	}
 
 	d = m;
-	ct = 1;
+	count = 1;
 
 	while (d > 9)
 	{
 		d /= 10;
-		ct *= 10;
+		count *= 10;
 	}
 
-	for (; ct >= 1; ct /= 10)
+	for (; count >= 1; count /= 10)
 	{
-		_putchar(((m / ct) % 10) + 48);
+		_putchar(((m / count) % 10) + 48);
 	}
 }
