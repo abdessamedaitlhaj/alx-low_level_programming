47738c02be20b5afeded1a456d901d19a52f37ce
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-02-23 14:53:35 -0800

    Print any given integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,42 +7,15 @@
  */
 void print_number(int n)
 {
-	int i, division, s = 1, size = 0;
+	int d;
 
-	division = n;
-
+	d = n;
 	if (n < 0)
 	{
-		putchar(45);
-		n = -1 * n;
+		putchar('-');
+		d = -n;
 	}
-	if (n == 0)
-	putchar('0');
-	else
-	{
-		while (division)
-		{
-			division = division / 10;
-			size++;
-		}
-
-		division = n;
-
-		for (i = 0; i < size - 1; i++)
-			s *= 10;
-		for (i = 0 ; i < size; i++)
-		{
-			division = n / s;
-
-			if (i == 0)
-			{
-				putchar(division + '0');
-				s /= 10;
-				continue;
-			}
-			putchar((division % 10) + '0');
-			s /= 10;
-	}
-	putchar('\n');
-	}
+	if (d / 10)
+		print_number(d / 10);
+	putchar((d % 10) + '0');
 }
