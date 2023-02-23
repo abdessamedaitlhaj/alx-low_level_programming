a475693f13bb7b0e1fd1f0dbf4474c17bc78b1be
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-02-23 04:36:54 -0800

    Draws a straight line in the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - Draws a straight line in the terminal
+ * @n: The number of underscore to print
+ *
+ * Description: Function that draws straigth line
+ * in the terminal
+ * Return: Void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+			_putchar('\n');
+		}
+}
