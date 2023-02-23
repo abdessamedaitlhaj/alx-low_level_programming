1979a3b15f4a793464b93b2cde2af68780dcee2f
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-02-23 10:31:27 -0800

    FizzBuzz Game

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,14 +16,20 @@ int main(void)
 	i = 1;
 	printf("%d",i);
 	for (i = 2; i <= 100; i++)
+	{
+		
 		if (i % 3 == 0 && i % 5 == 0)
 			printf(" FizzBuzz");
+		
 		else if (i % 3 == 0)
 			printf(" Fizz");
+		
 		else if (i % 5 == 0)
 			printf(" Buzz");
+		
 		else
 			printf(" %d", i);
+	}
 	printf("\n");
 	return (0);
 }
