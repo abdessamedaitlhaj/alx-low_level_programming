72ba6bde1ed255dd5c14e2e34407871541fa110a
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-02-27 15:03:27 -0800

    print even characters of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,14 @@ void puts2(char *str)
 {
 	char *p;
 
-	p = str;
-	while (*p != '\0')
+	int i = 0;
+
+
+	while (str[i] != '\0')
 	{
+		if (i % 2 == 0)
 		_putchar(*p);
-		p += 2;
+		i++;
 	}
 	_putchar('\n');
 }
