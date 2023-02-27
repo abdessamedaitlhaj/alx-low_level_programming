53460c9b1e126dd9421428dfc8068fd40488c951
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-02-27 15:15:06 -0800

    print last string half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 		i++;
 	}
 	if (length % 2 != 0)
-		for (i = (length - 1) / 2; i < length; i++)
+		for (i = (length - 1) / 2 + 1; i < length; i++)
 			_putchar(str[i]);
 	else
 		for (i = length / 2; i < length; i++)
