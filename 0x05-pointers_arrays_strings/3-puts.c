4f5110517e2b39efea93e4ab445c2e245fdf5bdc
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-02-27 09:07:44 -0800

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - print string
+ * @s: string to check
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+}
