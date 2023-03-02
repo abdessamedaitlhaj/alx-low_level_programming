211125c21d6384ea943d6c9b8b09575f75b740ee
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-02 07:13:56 -0800

    copies two strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @src: the string to be copied
+ * @dest: destination string
+ * @n: number of letters to copy
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *p, *q;
+
+	int length = 0;
+
+	for (q = src; *q != '\0'; q++, length++)
+	{}
+
+	if (n > length)
+		for (q = src, p = dest; *p != '\0'; p++, q++)
+			*p = *q;
+	else
+		for (q = src, p = dest; n > 0; p++, q++)
+		{
+			*p = *q;
+			n--;
+		}
+	return (dest);
+}
