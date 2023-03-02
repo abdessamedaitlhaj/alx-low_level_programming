4d6dc3466d75ca880ceab95d4518d7ef4c66cca2
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-02 15:09:41 -0800

    copy string to another one

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,10 +11,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; n; i++, n--)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		if (i < n && dest[i])
-			dest[i] = src[i];
+		dest[i] = src[i];
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
