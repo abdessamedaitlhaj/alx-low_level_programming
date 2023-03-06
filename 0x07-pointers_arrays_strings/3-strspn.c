de9538177f5665d922857ec31c2270d477349831
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-06 07:40:46 -0800

    gets the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to be checked
+ * @accept: string
+ *
+ * Return: length of the initial segement of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, counter = 0;
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				counter++;
+				break;
+			}
+		}
+	}
+	return (counter);
+}
