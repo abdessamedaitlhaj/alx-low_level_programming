5477b247cc17f01d7616e3d5fcb8b5dc8f3e9438
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-09 13:06:49 -0800

    is a string palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * len - length of string
+ * @s: string
+ *
+ * Return: length
+ */
+int len(char *s)
+{
+	if (*s)
+		return (1 + len(++s));
+	else
+		return (0);
+}
+
+/**
+ * pal - is string palindrome or not
+ * @s: string
+ * @i: counter
+ *
+ * Return: 1 if pal. 0 otherwise
+ */
+int pal(char *s, int i)
+{
+	int l;
+
+	l = len(s);
+	char *start = s;
+
+	char *end  = s + l - 1 - i;
+
+	if (i >= l / 2 && *start == *end)
+		return (1);
+	if (*start  == *end && start <= end)
+		return (pal(s + 1, ++i));
+	else
+		return (0);
+}
+
+/**
+ * is_palindrome - string is a palindrome or not.
+ * @s: string to be checked.
+ *
+ * Return: 1 if it is a palindrome. 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	return (pal(s, 0));
+}
