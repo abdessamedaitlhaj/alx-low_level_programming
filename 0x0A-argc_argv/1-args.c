84622e9e1e40eecc4407962504ed5e18ecae7b7b
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-10 03:39:47 -0800

    print the number of arguments passed in a command lin

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+/**
+ * main - print a program's name
+ * @argc: number of argument passed command line
+ * @argv: an array of strings
+ *
+ * Return: 0 Always
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+}
