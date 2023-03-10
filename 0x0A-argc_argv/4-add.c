a426a9ff249890cb399f815c2a79ad4ec9e27b5e
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-10 06:39:16 -0800

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,20 +10,23 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, sum = 0, f;
 
 	if (argc == 1)
 		printf("0\n");
 	else
 	{
 		for (i = 1; i < argc; i++)
-			if (strtol(argv[i], NULL, 10) == 0)
+		{
+			f = strtol(argv[i], NULL, 10);
+			if (f <= 0)
 			{
 				printf("Error\n");
 				return (1);
 			}
 			else
-				sum += strtol(argv[i], NULL, 10);
+				sum += f;
+		}
 		printf("%d\n", sum);
 	}
 	return (0);
