4572e07b60f6a0440af817ea11e5754de1299c9f
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-10 07:43:46 -0800

    print the number of parameters

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,10 @@
 /**
  * main - print a program's name
  * @argc: number of argument passed command line
- * @argv: an array of strings
  *
  * Return: 0 Always
  */
-int main(int argc, char *argv[])
+int main(int argc)
 {
 	printf("%d\n", argc - 1);
 	return (0);
