08e2bac835a026cade8e2df14326321cadd952dc
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-13 10:41:05 -0700

    concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,44 @@
 #include "main.h"
 
 /**
+ * len - length of a string.
+ * @s: string;
+ *
+ * Return: the length of a string.
+ */
+
+int len(char *s)
+{
+	int i = 0, length;
+
+	while (s[i])
+	{
+		length++;
+		i++;
+	}
+	return (length);
+}
+
+/**
+ * set - copy string.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: void
+ */
+void set(char *s1, char *s2)
+{
+	i = 0;
+	while (*s2)
+	{
+		s1[i] = *s2;
+		s2++;
+		i++;
+	}
+	s1[i] = '\0';
+	return (s1);
+}
+/**
  * str_concat - concatenate rwo strings.
  * @s1: first string.
  * @s2: second string.
@@ -12,24 +50,23 @@ char *str_concat(char *s1, char *s2)
 {
 	char *s1s2;
 	char *tmp1, *tmp2;
-	int len1, len2, i;
+	int i;
 
-	tmp1 = s1;
-	tmp2 = s2;
-	while (*tmp1)
-	{
-		len1++;
-		tmp1++;
-	}
-	while (*tmp2)
-	{
-		len2++;
-		tmp2++;
-	}
-	s1s2 = malloc(sizeof(char) * (len1 + len2 - 1));
+	s1s2 = malloc(sizeof(char) * (len(s1) + len(s2) - 1));
 
 	if (s1s2 == NULL)
 		return (NULL);
+
+	if (s1 == NULL && s2)
+	{
+		set(s1s2, s2);
+		return (s1s2);
+	}
+	if (s2 == NULL && s1)
+	{
+		set(s1s2, s1);
+		return (s1s2);
+	}
 	i = 0;
 	while (*s1)
 	{
