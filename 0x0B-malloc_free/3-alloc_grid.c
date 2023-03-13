43ab524fab499052c98f877498d1197747feb06c
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-13 14:16:00 -0700

    create 2d table

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of the 2d table.
+ * @heigth: heigth of 2d table.
+ *
+ * Return: returns a pointer to a 2d array of integers. NULL otherwise.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ar, i, j;
+
+	if(width <= 0 && height <= 0)
+                return (NULL);
+	ar = malloc(sizeof(int**) * width);
+	for (i = 0; i < width; i++)
+		ar[i] = malloc(sizeof(int*) * height);
+	for (i = 0; i < width; i++)
+		for (j = 0; j < height; j++)
+			ar[i][j] = 0;
+	return (ar);
+}
