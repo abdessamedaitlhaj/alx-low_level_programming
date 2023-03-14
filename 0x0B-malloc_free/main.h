75d922ed13c40be03240e3787dafb4e918164620
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-14 16:18:37 -0700

    header file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 char *create_array(unsigned int size, char c);
 int len(char *s);
-char *set(char *s1, char *s2, int j);
+char *set(char *s1, char *s2);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
