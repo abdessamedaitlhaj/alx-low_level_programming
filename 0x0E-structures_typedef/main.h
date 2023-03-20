d2942bdb6a410cb7749953dfd34afb693b60bee7
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-20 11:41:01 -0700

    header file

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,7 +1,7 @@
+#include "dog.h"
 #ifndef MAIN_H
 #define MAIN_h
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+void print_dog(struct dog *d);
 #endif
