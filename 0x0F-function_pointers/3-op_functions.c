d8491df09696e9df92ccc13cb379c18ff37129a2
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-22 18:55:29 -0700

    operation functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+/**
+ * op_add - adds two numbers.
+ * @a: first num.
+ * @b: second num.
+ *
+ * Return: the sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - the substruction of two numbers.
+ * @a: first num.
+ * @b: second num.
+ *
+ * Return: the result of substruction of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication of two numbers.
+ * @a: first num.
+ * @b: second num.
+ *
+ * Return: the multiplication of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: the division of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - the modulo of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: the result of modulo of a and b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
