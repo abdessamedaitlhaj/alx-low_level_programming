4846dd7b267f5e1f700af96ad07e12c97f57ad85
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-23 15:49:36 -0700

    print number separated with a string

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,13 +13,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
-
-	va_start(args, n);
-	if (separator == NULL)
-		for (i = 0; i < n; i++)
-			printf("%d", va_arg(args, int));
-	else
-		for (i = 0; i < n; i++)
-			printf("%d%s", va_arg(args, int), separator);
-	printf("\n");
+	if (n)
+	{
+		va_start(args, n);
+		if (separator == NULL)
+			for (i = 0; i < n; i++)
+				printf("%d", va_arg(args, int));
+		else
+			for (i = 0; i < n; i++)
+				printf("%d%s", va_arg(args, int), separator);
+		printf("\n");
+	}
 }
