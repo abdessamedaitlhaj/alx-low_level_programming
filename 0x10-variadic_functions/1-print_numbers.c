8195ef82152b753a43644e0886dd779787cd8316
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-23 15:47:51 -0700

    print number separated with a string

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers, followed by a new line.
+ * @separator: string to be printed between numbers.
+ * @n: number of args.
+ * @...: any given args.
+ *
+ * Return: nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	if (separator == NULL)
+		for (i = 0; i < n; i++)
+			printf("%d", va_arg(args, int));
+	else
+		for (i = 0; i < n; i++)
+			printf("%d%s", va_arg(args, int), separator);
+	printf("\n");
+}
