2f7a91607edadfb01fe77de4330476982c3af0ad
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-03-30 17:09:09 -0700

    return the number of elements of a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - prints all the elements of a list.
+ * @h: the list to be printed.
+ *
+ * Return: the list. NULL otherwise.
+ */
+size_t list_len(const list_t *h)
+{
+	int len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
