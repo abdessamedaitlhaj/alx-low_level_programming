528621472dfbc9cbb8dc0191fcf4e567d86e159b
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-04-04 03:47:40 -0700

    push a node to the end of a linked lis

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - push a node to the end of a linked list.
+ * @head: linked list to be modified.
+ * @n: the data.
+ *
+ * Return: the address of the new element. NULL otherwise.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *tmp;
+
+	tmp = *head;
+	new = malloc(sizeof(listint_t));
+	new->n = n;
+	new->next = NULL;
+	
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while(tmp)
+		{
+			if (tmp->next == NULL)
+			{
+				tmp->next = new;
+				break;
+			}
+			tmp = tmp->next;
+		}
+	}
+	return (*head);
+}
