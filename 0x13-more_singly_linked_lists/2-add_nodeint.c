4e112184236377e12e19bb6099942ded028d6ec9
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-04-04 03:04:34 -0700

    push new node to a linked list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of a linked list.
+ * @head: linked list.
+ *
+ * Return: the address of the new element. NULL otherwise.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		new->next = *head;
+		*head = new;
+	}
+	return (*head);
+}
