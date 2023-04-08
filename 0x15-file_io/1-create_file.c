442049455af4b235a96ebf279a3953fa6f892377
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-04-08 06:39:20 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: the name of the file to be created.
+ * @text_content: string to write to the file.
+ *
+ * Return: 1 on success. -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[i++])
+	{}
+
+	if (text_content == NULL)
+	{
+		if (write(fd, "", i) == -1)
+			return (-1);
+	}
+	else
+		if (write(fd, text_content, i) == -1)
+			return (-1);
+
+	if (close(fd) == -1)
+		return (-1);
+
+	return (1);
+}
