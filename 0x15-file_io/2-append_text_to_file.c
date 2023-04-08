c3215f16ebdbc8698fa19c46a277adac262c4d5b
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-04-08 09:21:07 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,8 +19,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while(*text_content++)
-		if(write(fd, text_content, 1) == -1)
+	while (*text_content++)
+		if (write(fd, text_content, 1) == -1)
 			return (-1);
 
 	if (close(fd) == -1)
