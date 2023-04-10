9d9093824883a3626f8f02476320ac24203405fc
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-04-10 10:56:18 -0700

    handle cp command line

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * close_fd - close file descriptor.
+ * @fd: file descriptor to be closed.
+ *
+ * Return: nothing.
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
  * main - handle cp command.
  * @argc: number of arguments.
  * @argv: the arguments passed to the command.
@@ -23,30 +37,32 @@ int main(int argc, char **argv)
 	fd1 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR |
 						S_IRGRP | S_IWGRP | S_IROTH);
 	fd2 = open(argv[1], O_RDONLY);
-
+	if (fd1 == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	if (fd2 == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	while ((n = read(fd2, buffer, sizeof(buffer))) > 0)
 	{
 		if (write(fd1, buffer, n) == -1)
 		{
+			close_fd(fd1);
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 	if (n == -1)
 	{
+		close_fd(fd2);
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(fd1) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd1);
-		exit(100);
-	}
-	if (close(fd2) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd2);
-		exit(100);
-	}
-
+	close_fd(fd1);
+	close_fd(fd2);
 	return (0);
 }
