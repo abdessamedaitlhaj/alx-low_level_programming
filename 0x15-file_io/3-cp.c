a50561bda3f83d5273676be8450fea1a403c4a3f
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-04-10 06:29:07 -0700

    handle cp command

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - handle cp command.
+ * @argc: number of arguments.
+ * @argv: the arguments passed to the command.
+ *
+ * Return: 0 success.
+ */
+int main(int argc, char **argv)
+{
+	int n, fd1, fd2;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp %s %s\n", argv[1], argv[2]);
+		exit(97);
+	}
+
+	fd1 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR |
+						S_IRGRP | S_IWGRP | S_IROTH);
+
+	fd2 = open(argv[1], O_RDONLY);
+
+	while ((n = read(fd2, buffer, sizeof(buffer))) != 0)
+	{
+		if (n == -1 || fd2 == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		if (write(fd1, buffer, n) == -1 || fd1 == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (close(fd1) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+
+	return (0);
+}
