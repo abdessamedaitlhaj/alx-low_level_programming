bd4d34d3f3643d5763c96d6de1ff8e7937f942a7
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-04-10 05:28:24 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0;
+	int fd, i = 0, n;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-
-	if (fd == -1)
-		return (-1);
-
-	while (text_content[i++])
-	{}
-
 	if (text_content == NULL)
-		return (1);
-
-	else
-		if (write(fd, text_content, i) == -1)
+	{
+		fd = open(filename, O_CREAT, S_IRUSR | S_IWUSR);
+		if (fd == -1)
 			return (-1);
+	}
+	else
+	{
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		if (fd == -1)
+			return (-1);
+		while (text_content[i++])
+		{}
+		n = write(fd, text_content, i);
+		if (n == -1)
+			return (-1);
+	}
 
 	if (close(fd) == -1)
 		return (-1);
-
 	return (1);
 }
