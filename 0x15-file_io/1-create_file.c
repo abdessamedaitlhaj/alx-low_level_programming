34464a4c044b7ea4a470d654fe24ce0d625c94f5
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-04-10 05:35:12 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,8 +25,8 @@ int create_file(const char *filename, char *text_content)
 		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 		if (fd == -1)
 			return (-1);
-		while (text_content[i++])
-		{}
+		while (text_content[i])
+		i++;
 		n = write(fd, text_content, i);
 		if (n == -1)
 			return (-1);
