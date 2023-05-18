18887a2b08f7f7f5a1567e28184904e0bd7bb89f
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-05-18 15:51:46 -0700

    largest number

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,8 +21,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (*h == NULL)
 	{
-		free(new);
-		return (NULL);
+		*h = new;
+		return (new);
 	}
 	if (idx == 0)
 	{
