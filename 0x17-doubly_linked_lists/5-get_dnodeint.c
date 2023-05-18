c54af54e330ab0e1477cdec29e57585823758984
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-05-18 06:36:05 -0700

    linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,6 +12,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i, len = 0;
 	dlistint_t *tmp = head;
 
+	if (head == NULL)
+		return (NULL);
+
 	while (head != NULL)
 	{
 		len++;
