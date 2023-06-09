29b774ee54ff08d7f74fd60e28b8b7de571f4647
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-09 08:44:45 -0700

    m

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,11 +40,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		tmp = tmp->next;
 	}
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = node;
-		return (1);
-	}
 	node->next = ht->array[index];
 	ht->array[index] = node;
 	return (1);
