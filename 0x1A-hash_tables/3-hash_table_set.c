332169b0b7f47300bc923b154b40aaf9c4d3ffc1
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-09 08:31:58 -0700

    m

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node;
+	hash_node_t *node, *tmp;
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
@@ -22,19 +22,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(node);
 		return (0);
 	}
-
-	index = hash_djb2((unsigned char *)key) % ht->size;
-
-	if (ht->array && strcmp(ht->array[index]->key, key) == 0)
-        {
-                free(ht->array[index]->key);
-                free(ht->array[index]->value);
-        }
 	node->key = malloc(strlen(key) + 1);
 	node->value = malloc(strlen(value) + 1);
 	strcpy(node->key, key);
 	strcpy(node->value, value);
 	node->next = NULL;
+
+	index = hash_djb2((unsigned char *)key) % ht->size;
+
+	tmp = ht->array[index];
+	while(tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+        	{
+			free(tmp->key);
+			free(tmp->value);
+			tmp->key = malloc(strlen(key) + 1);
+			tmp->value = malloc(strlen(value) + 1);
+			strcpy(tmp->key, key);
+			strcpy(tmp->value, value);
+			return (1);
+        	}
+		tmp = tmp->next;
+	}
 
 
 	if (ht->array[index] == NULL)
