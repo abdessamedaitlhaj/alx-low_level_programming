136dc926c4b968d3fb9a8356c1f5d555703670cd
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-09 07:54:03 -0700

    hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,19 +22,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(node);
 		return (0);
 	}
+
+	index = hash_djb2((unsigned char *)key) % ht->size;
+
+	if (ht->array && strcmp(ht->array[index]->key, key) == 0)
+        {
+                free(ht->array[index]->key);
+                free(ht->array[index]->value);
+        }
 	node->key = malloc(strlen(key) + 1);
 	node->value = malloc(strlen(value) + 1);
 	strcpy(node->key, key);
 	strcpy(node->value, value);
 	node->next = NULL;
 
-	index = hash_djb2((unsigned char *)key) % ht->size;
 
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = node;
 		return (1);
 	}
+
 	node->next = ht->array[index];
 	ht->array[index] = node;
 
