963f50185451f87ff37fca87f8c1dfb065c55bf7
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-09 04:23:32 -0700

    hash tables

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ * @ht: the hash table.
+ * @key: the key.
+ * @value: the value associated with the key.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node, *tmp;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+
+	if (key == NULL)
+		return (0);
+	node->key = strdup(key);
+	node->value = strdup(value)
+	node->next = NULL;
+
+	index = hash_djb2(key);
+
+	if (ht[index - 1] == NULL)
+		ht[index - 1] = node;
+
+	node->next = ht[index - 1];
+	ht[index - 1] = node;
+
+	return (1);
+}
