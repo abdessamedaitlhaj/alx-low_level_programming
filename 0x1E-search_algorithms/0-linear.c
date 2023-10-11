af05669fa4564f30cc0a3a74fcfbafb074cb3f75
“Abdessamed” <aitlhaj.abdessamed@gmail.com>
2023-10-11 14:31:03 -0700

    search algos

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,6 +6,8 @@
  * @array: a pointer to the first element of the array to search in
  * @size: the number of elements in array
  * @value: the value to search for
+ *
+ * Return: the first index where value is located
  */
 int linear_search(int *array, size_t size, int value)
 {
