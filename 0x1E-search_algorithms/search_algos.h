5df7b30e21e72afa862d564ac9a22ddb9dd7d312
“Abdessamed” <aitlhaj.abdessamed@gmail.com>
2023-10-25 14:42:45 -0700

    rm dir

--- 0x1E-search_algorithms/search_algos.h
+++
@@ -1,9 +0,0 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
-#include <stdio.h>
-
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-void print_t(int *array, size_t size);
-
-#endif
